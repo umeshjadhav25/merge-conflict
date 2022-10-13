290af8d98ca050d75d5b532270ac0fc532f670be
umesh <umesh>
2022-10-13 23:24:55 +0530

    Merged

--- main.c
+++ main.c
@@ -5,14 +5,11 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
 Initial Security()
 {
  //Sample Code
 }
 
-=======
->>>>>>> c2f1e6561c13ae29728df15dc2b3afc50c7dd6d7
 feature1()
 {
   //Sample Code for Feature 1
